0ece5d3dcfc41a94515499639a1cd61b9459d0f1
Muzzaki <103634066+ZzakyyNug@users.noreply.github.com>
2024-03-23 13:55:25 +0700

    Add files via upload

---
+++ Modul 2 Array/Unguided1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Nama: Muhammad Aulia Muzzaki Nugraha" << endl;
+    cout << "NIM : 2311102051" << endl;
+    
+    int numbers[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    cout << "Data array :";
+    for (int i = 0; i < 10; ++i) {
+        cout << " " << numbers[i];
+    }
+    cout << endl;
+
+    cout << "Nomor genap :";
+    for (int i = 0; i < 10; ++i) {
+        if (numbers[i] % 2 == 0) {
+            cout << " " << numbers[i];
+        }
+    }
+    cout << endl;
+
+    cout << "Nomor ganjil :";
+    for (int i = 0; i < 10; ++i) {
+        if (numbers[i] % 2 != 0) {
+            cout << " " << numbers[i];
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Modul 2 Array/Unguided2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Nama: Muhammad Aulia Muzzaki Nugraha" << endl;
+    cout << "NIM : 2311102051" << endl;
+    
+    int x_size, y_size, z_size;
+
+    cout << "Masukkan ukuran array (x y z): ";
+    cin >> x_size >> y_size >> z_size;
+
+    int arr[x_size][y_size][z_size];
+
+    cout << "Masukkan elemen array:\n";
+    for (int x = 0; x < x_size; x++) {
+        for (int y = 0; y < y_size; y++) {
+            for (int z = 0; z < z_size; z++) {
+                cout << "Array[" << x << "][" << y << "][" << z << "]: ";
+                cin >> arr[x][y][z];
+            }
+        }
+    }
+
+    cout << "Elemen array yang dimasukkan:\n";
+    for (int x = 0; x < x_size; x++) {
+        for (int y = 0; y < y_size; y++) {
+            for (int z = 0; z < z_size; z++) {
+                cout << arr[x][y][z] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
